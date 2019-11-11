4914e2f71282837b98c5d770a70a2f0394474505
objectGrzegorz <pocztamilanowski@gmail.com>
2019-11-11 12:34:51 +0100

    Update Słowo Piknik.py
    Poprawione nazwy, zmieniony sposób otwierania pliku słownika

--- Słowo Piknik.py
+++ Słowo Piknik.py
@@ -1,49 +1,49 @@
 import random,copy
 
-#Tworzenie listy liter
-def listaliter():
+#Tworzenie listy liter, z których będą tworzone słowa
+def lista_liter():
     litery=[]
     lit=input("Podaj litery"+"\n")
     while lit!="":
         litery.append(lit)
-        lit = input("Podaj litery"+"\n")
+        lit = input("Podaj literę albo daj 'enter' aby przejść dalej"+"\n")
     return litery
 
 #Funkcja tworzenia jednego słowa
-def losuj(litery2,ileliter):
+def losuj(litery_kopia,ile_liter):
     slowo=""
-    for i in range(ileliter):
-        litera = random.choice(litery2)
+    for i in range(ile_liter):
+        litera = random.choice(litery_kopia)
         slowo += litera
-        litery2.remove(litera)
+        litery_kopia.remove(litera)
     return slowo
 
 #Pętla programu
-czyszukac=input("Jeśli chcesz szukać wyrazu wpisz 't'")
-litery=listaliter()
+czy_szukac=input("Jeśli chcesz szukać wyrazu wpisz 't'")
+litery=lista_liter()
 
-while czyszukac.lower() == "t":
-    listaslow = []
-    ileliter=int(input("Ilu literowego słowa szukasz?"+"\n"))
-    litery2=copy.copy(litery)
+while czy_szukac.lower() == "t":
+    lista_slow = []
+    ile_liter=int(input("Ilu literowego słowa szukasz?"+"\n"))
+    litery_kopia=copy.copy(litery)
     flaga=True
     licznik=0
     while flaga:
-        wyraz=losuj(litery2, ileliter)
-        litery2 = copy.copy(litery)
-        if wyraz not in listaslow:
-            listaslow.append(wyraz)
+        wyraz=losuj(litery_kopia, ile_liter)
+        litery_kopia = copy.copy(litery)
+        if wyraz not in lista_slow:
+            lista_slow.append(wyraz)
         else:
             licznik+=1
         if len(litery)<=10 and licznik==1000 or len(litery)<=16 and licznik==10000 or len(litery)>16 and licznik==100000:
             flaga=False
 
     with open("slowa.txt","r",encoding="utf-8") as plik:
-        linia=plik.readline().strip()
-        while linia !="":
-            if linia in listaslow:
+        for linia in plik:
+            linia=linia.rstrip("\n")
+            if linia in lista_slow:
                 print(linia)
-            linia = plik.readline().strip()
+
 
     czyszukac = input("Jeśli chcesz szukać wyrazu kliknij 't'")
 
