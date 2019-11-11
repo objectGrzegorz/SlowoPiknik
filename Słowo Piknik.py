bb7ce937dc63677110ee5ccf917a078b61f79e09
objectGrzegorz <pocztamilanowski@gmail.com>
2019-11-11 12:52:48 +0100

    Update Słowo Piknik.py
    Dodano opuszczanie programu, jeśli użytkownik wprowadzi złe dane.

--- Słowo Piknik.py
+++ Słowo Piknik.py
@@ -1,9 +1,12 @@
-import random,copy
+import random,copy,sys
 
-#Tworzenie listy liter, z których będą tworzone słowa
+#Tworzenie listy liter, z których powstaną słowa
 def lista_liter():
     litery=[]
     lit=input("Podaj litery"+"\n")
+    if not lit.isalpha():
+        print("Nie podałeś litery")
+        sys.exit()
     while lit!="":
         litery.append(lit)
         lit = input("Podaj literę albo daj 'enter' aby przejść dalej"+"\n")
@@ -20,11 +23,18 @@ def losuj(litery_kopia,ile_liter):
 
 #Pętla programu
 czy_szukac=input("Jeśli chcesz szukać wyrazu wpisz 't'")
+if czy_szukac.lower() != "t":
+    sys.exit()
 litery=lista_liter()
 
 while czy_szukac.lower() == "t":
     lista_slow = []
-    ile_liter=int(input("Ilu literowego słowa szukasz?"+"\n"))
+    print("Ilu literowego słowa szukasz?"+"\n")
+    ile_liter = input()
+    if ile_liter.isdigit():
+        ile_liter = int(ile_liter)
+    else:
+        sys.exit()
     litery_kopia=copy.copy(litery)
     flaga=True
     licznik=0
@@ -37,15 +47,14 @@ while czy_szukac.lower() == "t":
             licznik+=1
         if len(litery)<=10 and licznik==1000 or len(litery)<=16 and licznik==10000 or len(litery)>16 and licznik==100000:
             flaga=False
-
     with open("slowa.txt","r",encoding="utf-8") as plik:
         for linia in plik:
             linia=linia.rstrip("\n")
             if linia in lista_slow:
                 print(linia)
-
-
-    czyszukac = input("Jeśli chcesz szukać wyrazu kliknij 't'")
+    czy_szukac = input("Jeśli chcesz szukać wyrazu kliknij 't'")
+    if czy_szukac.lower() != "t":
+        sys.exit()
 
 
 
